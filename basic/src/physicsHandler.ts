827cf93c0e136ca5ec179e978e1215feca36edef
Hans Beemsterboer <hpbeem@gmail.com>
2019-01-10 20:22:48 +0100

    Add license info

--- basic/src/physicsHandler.ts
+++ basic/src/physicsHandler.ts
@@ -1,3 +1,31 @@
+/*
+ * Copyright 2019 Hans Beemsterboer
+ *
+ * This file has been modified by Hans Beemsterboer to be used in
+ * the webxr-physics project.
+ *
+ * Copyright (c) 2015 cannon.js Authors
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import {Body, NaiveBroadphase, PointToPointConstraint, Quaternion, Sphere, Vec3, World} from "cannon";
 import {Mesh, Scene, Vector3} from "three";
 import RayInput from './ray-input/ray-input';
@@ -21,18 +49,17 @@ export default class PhysicsHandler {
     let world;
 
     // To be synced
-      let bodies: Body[] = new Array<Body>();
-      this.meshes = new Array<Mesh>();
+    let bodies: Body[] = new Array<Body>();
+    this.meshes = new Array<Mesh>();
     this.bodies = bodies;
     let axes = [];
-    axes[ 0 ] = {
-      value: [ 0, 0 ]
+    axes[0] = {
+      value: [0, 0]
     };
 
     world = new World();
     world.quatNormalizeSkip = 0;
     world.quatNormalizeFast = false;
-    //
     world.gravity.set(0, 0 ,0);
     world.broadphase = new NaiveBroadphase();
 
@@ -95,18 +122,18 @@ export default class PhysicsHandler {
     // Vector to the clicked point, relative to the body
     let v1 = new Vec3(x,y,z).vsub(this.constrainedBody.position);
 
-    // // Apply anti-quaternion to vector to transform it into the local body coordinate system
+    // Apply anti-quaternion to vector to transform it into the local body coordinate system
     let antiRot = this.constrainedBody.quaternion.inverse();
     let pivot = new Quaternion(antiRot.x, antiRot.y, antiRot.z, antiRot.w).vmult(v1); // pivot is not in local body coordinates
 
-    // // Move the cannon click marker particle to the click position
+    // Move the cannon click marker particle to the click position
     this.jointBody.position.set(x,y,z);
-    //
-    // // Create a new constraint
-    // // The pivot for the jointBody is zero
+
+    // Create a new constraint
+    // The pivot for the jointBody is zero
     this.pointerConstraint = new PointToPointConstraint(this.constrainedBody, pivot, this.jointBody, new Vec3(0,0,0));
-    //
-    // // Add the constraint to world
+
+    // Add the constraint to world
     this.world.addConstraint(this.pointerConstraint);
   }
 
