74940ffbb343141153d97a97007eec7ccbb99f85
Hans Beemsterboer <hpbeem@gmail.com>
2019-05-05 18:47:36 +0200

    Download resource from experiment root

--- experiments/basic/src/audio/audioHandler.ts
+++ experiments/basic/src/audio/audioHandler.ts
@@ -1,6 +1,6 @@
 import {ResonanceAudio, Source} from "resonance-audio";
 // @ts-ignore
-import cubeSound from '../../../../js/resources/cube-sound.wav';
+import cubeSound from '../../cube-sound.wav';
 
 export default class AudioHandler {
   
